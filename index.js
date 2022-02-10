f16a713bc56e7f3715d4dda3b63dbcd4ea756c6e
GwonM <hideb36577@naver.com>
2022-02-10 22:58:05 +0900

    비밀 정보 보호

--- index.js
+++ index.js
@@ -3,14 +3,33 @@ const app = express();
 const port = 3000;
 
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const config = require("./config/key");
+
+const { user, User } = require("./models/User");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 mongoose
-    .connect("mongodb+srv://Lee:Wndn%40530@boiler-plate.f7ipx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {})
+    .connect(config.mongoURI, {})
     .then(() => console.log("MongoDB Connected..."))
     .catch((err) => console.log(err));
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("Hello World! 2022");
+});
+
+app.post("/register", (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, doc) => {
+        if (err) return res.json({ success: false, err });
+        return res.status(200).json({
+            success: true,
+        });
+    });
 });
 
 app.listen(port, () => {
